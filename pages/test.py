86b61cac8459c926bdca4b7bdfa85ac437c37996
drajayshukla <39010063+drajayshukla@users.noreply.github.com>
2024-12-09 09:50:34 +0530

    x
    x

--- pages/test.py
+++ pages/test.py
@@ -1,11 +1,2 @@
-from pathlib import Path
-
-from pathlib import Path
-
-font_path = Path(__file__).parent / "DejaVuSans.ttf"  # Searches in the script's directory
-
-if not font_path.exists():
-    raise FileNotFoundError(f"{font_path} font file not found! Please add it to the project folder.")
-
-# Load the font into FPDF
-self.add_font("DejaVu", style="", fname=str(font_path), uni=True)
+import os
+print("Current working directory:", os.getcwd())
