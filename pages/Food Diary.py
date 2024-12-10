a7b27cce219ce69793f8125406b545368d9e3b70
drajayshukla <39010063+drajayshukla@users.noreply.github.com>
2024-12-10 22:54:31 +0530

    Update Food Diary.py
    x

--- pages/Food Diary.py
+++ pages/Food Diary.py
@@ -2,8 +2,28 @@ import streamlit as st
 from fpdf import FPDF
 import tempfile
 
+import re
 
-# Save data to PDF
+
+def remove_emojis(text):
+    emoji_pattern = re.compile(
+        "["
+        "\U0001F600-\U0001F64F"  # emoticons
+        "\U0001F300-\U0001F5FF"  # symbols & pictographs
+        "\U0001F680-\U0001F6FF"  # transport & map symbols
+        "\U0001F700-\U0001F77F"  # alchemical symbols
+        "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+        "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+        "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+        "\U0001FA00-\U0001FA6F"  # Chess Symbols
+        "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+        "\U00002702-\U000027B0"  # Dingbats
+        "]+",
+        flags=re.UNICODE,
+    )
+    return emoji_pattern.sub(r"", text)
+
+
 def save_to_pdf(data, notes, rewards):
     pdf = FPDF()
     pdf.add_page()
@@ -13,26 +33,26 @@ def save_to_pdf(data, notes, rewards):
 
     for day, meals in data.items():
         pdf.set_font("Arial", size=14)
-        pdf.cell(200, 10, txt=f"{day}", ln=True)
+        pdf.cell(200, 10, txt=remove_emojis(f"{day}"), ln=True)
         pdf.ln(5)
         pdf.set_font("Arial", size=12)
         for meal in meals:
             for key, value in meal.items():
-                pdf.cell(0, 10, txt=f"{key}: {value}", ln=True)
+                pdf.cell(0, 10, txt=remove_emojis(f"{key}: {value}"), ln=True)
             pdf.ln(5)
 
     pdf.set_font("Arial", size=14)
     pdf.cell(200, 10, txt="Additional Notes", ln=True)
     pdf.set_font("Arial", size=12)
     for key, value in notes.items():
-        pdf.cell(0, 10, txt=f"{key}: {value}", ln=True)
+        pdf.cell(0, 10, txt=remove_emojis(f"{key}: {value}"), ln=True)
 
     pdf.ln(10)
     pdf.set_font("Arial", size=14)
     pdf.cell(200, 10, txt="Rewards Earned", ln=True)
     pdf.set_font("Arial", size=12)
     for reward in rewards:
-        pdf.cell(0, 10, txt=f"- {reward}", ln=True)
+        pdf.cell(0, 10, txt=remove_emojis(f"- {reward}"), ln=True)
 
     temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
     pdf.output(temp_file.name)
